0e1cedfa741fb00c921086118984caf40370a2ce
Nabin Jana <120771456+nabinjana-dsc@users.noreply.github.com>
2024-02-25 16:21:36 +0530

    files added

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = "10"
+
+//console.log(a + + b)
+
+//== value compare
+//=== value + data type
+if(a === b){
+    console.log("1st answer")
+    }else{
+        console.log('2nd answer')
+}
